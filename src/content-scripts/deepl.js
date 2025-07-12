1dafb8870494f21716e0c8aebb704b480e07bc23
Stoyan <stoyank127@gmail.com>
2025-07-12 14:17:42 +0300

    improve readability of DeepL content script
    Signed-off-by: Stoyan <stoyank127@gmail.com>

--- src/content-scripts/deepl.js
+++ src/content-scripts/deepl.js
@@ -1,26 +1,14 @@
-alert('ding');
-console.log('deepl content script loaded');
-const xpathTarget =
+// XPath for the translation text area containing the Bulgarian sentence.
+const translationTextXPath =
     '/html/body/div[1]/div[1]/div[2]/div[2]/div[1]/div[2]/div[1]/main/div[2]/nav/div/div[2]/div/div/div[1]/div/div/div/div/div/div/section/div/div[2]/div[3]/section/div[1]/d-textarea';
 
-function getValue(xpath) {
-    const result = document.evaluate(
-        xpath,
-        document,
-        null,
-        XPathResult.STRING_TYPE,
-        null
-    );
-    return result.stringValue.trim();
-}
-
 function waitForText() {
-    const observer = new MutationObserver((mutations, obs) => {
-        const text = getValue(xpathTarget);
-        if (text) {
-            browser.storage.local.set({ bulgarianSentence: text }).then(() => {
-                browser.runtime.sendMessage({ type: 'data-updated' });
-            });
+    const observer = new MutationObserver(async (mutations, obs) => {
+        const bulgarianSentence =
+            window.getStringFromXPath(translationTextXPath);
+        if (bulgarianSentence) {
+            await browser.storage.local.set({ bulgarianSentence });
+            await browser.runtime.sendMessage({ type: 'data-updated' });
 
             obs.disconnect();
         }
@@ -32,8 +20,4 @@ function waitForText() {
     });
 }
 
-if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', waitForText);
-} else {
-    waitForText();
-}
+waitForText();
