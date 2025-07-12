5ac03b02a5cf39b80a5af93a42fb72b22bb704d3
Stoyan <stoyank127@gmail.com>
2025-07-12 14:33:45 +0300

    use 'main' for consistency

--- src/content-scripts/deepl.js
+++ src/content-scripts/deepl.js
@@ -2,7 +2,7 @@
 const translationTextXPath =
     '/html/body/div[1]/div[1]/div[2]/div[2]/div[1]/div[2]/div[1]/main/div[2]/nav/div/div[2]/div/div/div[1]/div/div/div/div/div/div/section/div/div[2]/div[3]/section/div[1]/d-textarea';
 
-function waitForText() {
+function main() {
     const observer = new MutationObserver(async (mutations, obs) => {
         const bulgarianSentence =
             window.getStringFromXPath(translationTextXPath);
@@ -20,4 +20,4 @@ function waitForText() {
     });
 }
 
-waitForText();
+main();
