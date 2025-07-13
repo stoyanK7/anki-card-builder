a34ffcfb75a0ec839950be4b0433fc46673635fc
Stoyan <stoyank127@gmail.com>
2025-07-13 19:57:48 +0300

    extract card building process in shared module

---
+++ src/shared/card-workflow.js
@@ -0,0 +1,40 @@
+export async function startCardBuildingProcess(frenchWord) {
+    openCardBuilderInPopupWindow();
+    openResourceTabsForFrenchWord(frenchWord);
+}
+
+/**
+ * Open the card builder panel in a popup window. The window will
+ * always be on top of the other windows.
+ */
+async function openCardBuilderInPopupWindow() {
+    browser.windows.create({
+        url: browser.runtime.getURL('src/card-builder/card-builder.html'),
+        type: 'popup',
+        width: 350,
+        height: 560,
+        focused: true
+    });
+}
+
+/**
+ * Open multiple tabs with resources related to the given French word.
+ * One website contains an audio file with the pronunciation of the word,
+ * another website contains an example sentence with the word, and so on.
+ * The manifest.json file maps the URLs to content scripts that will
+ * scrape the data.
+ *
+ * @param {string} frenchWord
+ */
+async function openResourceTabsForFrenchWord(frenchWord) {
+    const encodedURIfrenchWord = encodeURIComponent(frenchWord);
+    const urls = [
+        `https://fr.wiktionary.org/wiki/${encodedURIfrenchWord}`,
+        `https://translate.google.com/?sl=fr&tl=bg&text=${encodedURIfrenchWord}&op=translate`,
+        `https://www.google.com/search?tbm=isch&q=${encodedURIfrenchWord}`,
+        `https://www.collinsdictionary.com/sentences/french/${encodedURIfrenchWord}`
+    ];
+    urls.forEach((url) => {
+        browser.tabs.create({ url });
+    });
+}
