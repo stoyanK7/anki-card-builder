829bb42a449dd35dc074b21be885fbc0f47f4efe
Stoyan <stoyank127@gmail.com>
2025-07-13 20:32:57 +0300

    improve readability of invokeAnkiConnect

--- src/shared/anki-connect.js
+++ src/shared/anki-connect.js
@@ -1,5 +1,20 @@
-// Taken from https://github.com/amikey/anki-connect?tab=readme-ov-file#sample-invocation
-// and modified to use fetch API instead of XMLHttpRequest
+/**
+ * Invoke an Anki Connect action with the given parameters.
+ *
+ * This function assumes that Anki Connect is running and listening on
+ * http://127.0.1:8765.
+ *
+ * This function is inspired by
+ * https://github.com/amikey/anki-connect?tab=readme-ov-file#sample-invocation
+ * and was modified to use the fetch API instead of XMLHttpRequest.
+ *
+ * @param {string} action Anki Connect action to invoke.
+ *                        For example, "addNote", "findNotes", etc.
+ * @param {object} params Parameters for the Anki Connect action.
+ * @returns {Promise<any>} The result field of the Anki Connect response.
+ *                         The type of the result depends on the action.
+ * @throws {Error} If the response is not valid or if Anki Connect returns an error.
+ */
 export function invokeAnkiConnect(action, params = {}) {
     const ankiConnectVersion = 6;
 
@@ -10,27 +25,37 @@ export function invokeAnkiConnect(action, params = {}) {
         },
         body: JSON.stringify({ action, version: ankiConnectVersion, params })
     })
-        .then((res) => {
-            if (!res.ok) {
+        .then((response) => {
+            if (!response.ok) {
                 throw new Error(
                     'Network response was not ok. Status: ' + res.status
                 );
             }
-            return res.json();
+
+            return response.json();
         })
-        .then((response) => {
-            if (Object.keys(response).length !== 2) {
-                throw new Error('Response has an unexpected number of fields');
+        .then((responseObject) => {
+            if (Object.keys(responseObject).length !== 2) {
+                throw new Error(
+                    `Response has an unexpected number of fields. Expected 2 fields but got ${
+                        Object.keys(responseObject).length
+                    }.`
+                );
             }
-            if (!('error' in response)) {
-                throw new Error('Response is missing required "error" field');
+            if (!('error' in responseObject)) {
+                throw new Error('Response is missing required "error" field.');
             }
-            if (!('result' in response)) {
-                throw new Error('Response is missing required "result" field');
+            if (!('result' in responseObject)) {
+                throw new Error('Response is missing required "result" field.');
             }
-            if (response.error) {
-                throw new Error(response.error);
+            if (responseObject.error) {
+                throw new Error(responseObject.error);
             }
-            return response.result;
+
+            // Anki Connect returns an object but we only need the result field.
+            return responseObject.result;
+        })
+        .catch((error) => {
+            throw new Error('AnkiConnect request failed: ' + error.message);
         });
 }
