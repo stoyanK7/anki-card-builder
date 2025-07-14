b7b9347987f6f32b1f80529225439de30fbbf6ac
Stoyan <stoyank127@gmail.com>
2025-07-14 20:49:26 +0300

    add context menu option to use an image for the deck

--- src/background.js
+++ src/background.js
@@ -41,6 +41,12 @@ browser.contextMenus.create({
     contexts: ['selection']
 });
 
+browser.contextMenus.create({
+    id: 'use-image',
+    title: '👆 Use Image',
+    contexts: ['image']
+});
+
 browser.contextMenus.onClicked.addListener((info, tab) => {
     if (info.menuItemId === 'anki-prepare-card' && info.selectionText) {
         const frenchWord = info.selectionText.trim();
@@ -63,4 +69,19 @@ browser.contextMenus.onClicked.addListener((info, tab) => {
         browser.storage.local.set({ frenchWord });
         startCardBuildingProcess(info.selectionText.trim());
     }
+    if (info.menuItemId === 'use-image') {
+        handleUseImageContextMenu(info);
+    }
 });
+
+function handleUseImageContextMenu(info) {
+    if (info && info.mediaType !== 'image') {
+        console.warn('Context menu clicked on non-image element:', info);
+        return;
+    }
+    if (!info.srcUrl) {
+        console.warn('No image source URL found in context menu info:', info);
+        return;
+    }
+    browser.storage.local.set({ imageSrc: info.srcUrl });
+}
