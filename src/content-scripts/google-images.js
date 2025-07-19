5d62c49ba4449923d85e97b8e191b663ab8fa196
Stoyan <stoyank127@gmail.com>
2025-07-19 16:08:59 +0300

    go the extra mile of selecting the high quality version of an image by default

--- src/content-scripts/google-images.js
+++ src/content-scripts/google-images.js
@@ -1,5 +1,8 @@
 const imageTilesXPath =
     '/html/body/div[3]/div/div[14]/div/div[2]/div[2]/div/div/div/div/div[1]/div/div/div';
+const highQualityImageXPath =
+    '/html/body/div[15]/div[2]/div[3]/div/div/c-wiz/div/div[2]/div[2]/div/div[2]/c-wiz/div/div[2]/div[1]/a/img[1]';
+let firstImgClicked = false;
 
 const observer = new MutationObserver(() => {
     const result = document.evaluate(
@@ -14,10 +17,40 @@ const observer = new MutationObserver(() => {
         const firstTile = result.snapshotItem(0);
         const firstImg = firstTile.querySelector('img');
 
-        if (firstImg && firstImg.src) {
-            browser.storage.local.set({ imageSrc: firstImg.src });
-            observer.disconnect();
+        if (!firstImg) {
+            return;
         }
+
+        // We click the image because we want the high quality version of the image.
+        // All images in the grid view are low quality.
+        // By clicking that image, it will open the high quality version of the image
+        // on the right side of the screen and we can then scrape it.
+        firstImg.click();
+        firstImgClicked = true;
+    }
+
+    if (!firstImgClicked) {
+        return;
+    }
+
+    const highQualityImageResult = document.evaluate(
+        highQualityImageXPath,
+        document,
+        null,
+        XPathResult.FIRST_ORDERED_NODE_TYPE,
+        null
+    );
+    const highQualityImage = highQualityImageResult.singleNodeValue;
+
+    if (
+        highQualityImage &&
+        highQualityImage.src &&
+        // Google places 2 images in a div, one above the other. They have a gstatic.com as
+        // a backup image. We want the non-gstatic image which is the high quality one.
+        !highQualityImage.src.includes('gstatic.com')
+    ) {
+        browser.storage.local.set({ imageSrc: highQualityImage.src });
+        observer.disconnect();
     }
 });
 
