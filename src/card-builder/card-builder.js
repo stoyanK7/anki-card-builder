2f7518a928f39e1a905d2aa76f7a173000b2a14a
Stoyan <stoyank127@gmail.com>
2025-08-04 20:28:13 +0300

    improve prompt to LLM

--- src/card-builder/card-builder.js
+++ src/card-builder/card-builder.js
@@ -356,16 +356,18 @@ function reloadFrenchSentence() {
         headers: {
             'Content-Type': 'application/json'
         },
-        // TODO: Fix the prompt to be more specific.
         body: JSON.stringify({
             model: 'jobautomation/OpenEuroLLM-French',
-            prompt: 'Donne uniquement une phrase en français, niveau '
-            + `A1-A2, avec le mot suivant : ${frenchWord}. La phrase `
-            + 'doit avoir 10 mots ou moins. Ne réponds qu\'avec la phrase,' 
-            + 'sans explication ni introduction.',
+            prompt: 'Donne uniquement une phrase en français, niveau A1-A2, '
+            + `avec le mot suivant : ${frenchWord}. La phrase doit avoir 10 `
+            + 'mots ou moins. Ne réponds qu\'avec la phrase, sans explication '
+            + 'ni introduction. Exemple : Si le mot est "chat", réponds : "Le '
+            + ' chat dort sur le canapé."',
             stream: false,
             options: {
-                temperature: 0.8
+                temperature: 0.5,
+                top_k: 30, // eslint-disable-line camelcase
+                top_p: 0.5 // eslint-disable-line camelcase
             }
         })
     })
