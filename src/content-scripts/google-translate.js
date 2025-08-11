b12b308fe37581e27656623252b4dea04d4890ff
Stoyan <stoyank127@gmail.com>
2025-08-11 18:42:01 +0300

    make Google Translate scraper work with browser messages

--- src/content-scripts/google-translate.js
+++ src/content-scripts/google-translate.js
@@ -1,16 +1,74 @@
-const wordTranslationXPath =
-    '/html/body/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]'
-    + '/div[2]/div[2]/c-wiz/div/div[6]/div/div[1]/span[1]/span/span';
+main();
 
-const observer = new MutationObserver(async (mutations, obs) => {
-    const bulgarianWord = getStringFromXPath(wordTranslationXPath);
-    if (bulgarianWord) {
-        browser.storage.local.set({ bulgarianWord });
-        obs.disconnect();
-    }
-});
+function main() {
+    orchestrateBulgarianWordScraping();
+}
 
-observer.observe(document.body, {
-    childList: true,
-    subtree: true
-});
+/**
+ * Orchestrate scraping the Bulgarian word
+ * and send progress/results via browser messages.
+ */
+function orchestrateBulgarianWordScraping() {
+    browser.runtime.sendMessage({
+        action: 'scrape-start',
+        parameter: 'bulgarianWord'
+    })
+        .then(() => {
+            return scrapeBulgarianWord();
+        })
+        .then(bulgarianWord => {
+            browser.runtime.sendMessage({
+                action: 'scrape-success',
+                parameter: 'bulgarianWord',
+                value: bulgarianWord
+            });
+        })
+        .catch(error => {
+            browser.runtime.sendMessage({
+                action: 'scrape-error',
+                parameter: 'bulgarianWord',
+                error: error
+            });
+        });
+}
+
+/**
+ * Scrape the Bulgarian (translation) word from the Google Translate page.
+ *
+ * @returns {Promise<string>} The Bulgarian word extracted from the page.
+ * @throws {Error} If the Bulgarian word is not found within 8 seconds.
+ */
+async function scrapeBulgarianWord() {
+    const bulgarianWordXPath =
+        '/html/body/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]'
+        + '/div[2]/div[2]/c-wiz/div/div[6]/div/div[1]/span[1]/span/span';
+
+    return new Promise((resolve, reject) => {
+        const observer = new MutationObserver(async (mutations, obs) => {
+            try {
+                const bulgarianWord = getStringFromXPath(bulgarianWordXPath);
+                if (bulgarianWord) {
+                    clearTimeout(timeoutId);
+                    observer.disconnect();
+                    resolve(bulgarianWord);
+                }
+            } catch(error) {
+                /**
+                 * Do nothing, continue observing.
+                 * getStringFromXPath() will throw an error if the element
+                 * is not found but that is expected until the page loads.
+                 */
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+
+        const timeoutId = setTimeout(() => {
+            observer.disconnect();
+            reject(new Error('Bulgarian word not found within 8 seconds.'));
+        }, 8000);
+    });
+}
